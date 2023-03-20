7b4dc201b02d7feedd9b684c3fac366cbe646d74
Pravin Bhagwat <85389806+pravinbhagwat@users.noreply.github.com>
2023-03-20 16:51:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
-function letterCombinations(input_digit) {
+function letterCombinations(digit) {
   //Complete the function
+	if (!digits) {
+    return [];
+  }
+  
+  const map = {
+    "2": "abc",
+    "3": "def",
+    "4": "ghi",
+    "5": "jkl",
+    "6": "mno",
+    "7": "pqrs",
+    "8": "tuv",
+    "9": "wxyz"
+  };
+  
+  const result = [];
+  
+  function backtrack(combination, nextDigits) {
+    if (nextDigits.length === 0) {
+      result.push(combination);
+    } else {
+      const digit = nextDigits[0];
+      const letters = map[digit];
+      for (let i = 0; i < letters.length; i++) {
+        const letter = letters[i];
+        backtrack(combination + letter, nextDigits.slice(1));
+      }
+    }
+  }
+  
+  backtrack("", digits);
+  
+  return result.sort();
 }
 
 module.exports = letterCombinations;
